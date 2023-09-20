91934cba3aa148015ee9c8382638d026f4c802c2
Georgia Bjärstål <georgia.bjarstal@epita.fr>
2023-09-20 20:16:58 +0200

    choosing user between existing ones

--- expense.py
+++ expense.py
@@ -2,6 +2,11 @@ from PyInquirer import prompt
 
 import csv
 
+def get_spender_choices():
+    with open("users.csv", "r") as f:
+        reader = csv.reader(f)
+        return [row[0] for row in reader]
+
 expense_questions = [
     {
         "type":"input",
@@ -14,19 +19,17 @@ expense_questions = [
         "message":"New Expense - Label: ",
     },
     {
-        "type":"input",
+        "type":"list",
         "name":"spender",
         "message":"New Expense - Spender: ",
+        "choices": get_spender_choices()
     },
-
 ]
-
-
 
 def new_expense(*args):
     infos = prompt(expense_questions)
 
-    with open("expense_report.csv", "w", newline="") as f:
+    with open("expense_report.csv", "a", newline="") as f:
         reader = csv.writer(f)
         amount = infos.get("amount")
         label = infos.get("label")
